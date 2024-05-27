4c655b74243ecaf0996c513e70e685bf415c1496
julian <juliansolo7189@gmail.com>
2024-05-27 13:43:03 -0500

    ejercicios javascript

---
+++ main.js
@@ -0,0 +1,25 @@
+// Definimos la función "sumar" que toma dos argumentos, "a" y "b", y devuelve su suma.
+function sumar(a, b) {
+    return a + b;
+  }
+  
+  // Definimos la función "multiplicar" que toma dos argumentos, "a" y "b", y devuelve su producto.
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  // Definimos la función "dividir" que toma dos argumentos, "a" y "b", y devuelve su cociente.
+  function dividir(a, b) {
+    return a / b;
+  }
+  
+  // Definimos la función "concatenar" que toma dos argumentos, "a" y "b", y devuelve una cadena que es la concatenación de ambos.
+  function concatenar(a, b) {
+    return a + " " + b;
+  }
+  
+  // Definimos la función "comparar" que toma dos argumentos, "a" y "b", y devuelve verdadero si son iguales y falso si no lo son.
+  function comparar(a, b) {
+    return a === b;
+  }
+  
